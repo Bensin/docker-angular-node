8564acc1d301e847d271a0a50ac1bea6827d1e8a
bensin <bensinpb@gmail.com>
2020-05-18 13:00:51 +1000

    initial commit

---
+++ node-api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.listen(PORT, ()=> {
+    console.log(`server listening port ${PORT}`);
+})
+
